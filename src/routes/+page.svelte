5911f4bea5c7a1ade20e6139c12820d53d1b971a
Supern_Ice <alfietompsett@outlook.com>
2025-12-08 23:04:24 +0000

    Added more to the main joke page and styles for the serious page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,26 +9,32 @@
 	<title>Graphic Design is My Passion</title>
 </svelte:head>
 
-<div class="w-full max-w-dvw overflow-x-hidden bg-linear-to-r from-yellow-300 to-green-500 pb-4" style="z-index: 1">
+<div class="relative z-0 h-full w-full max-w-dvw overflow-x-hidden bg-linear-to-r from-yellow-300 to-green-500 pb-4">
+	<img src="link-fall.gif" alt="Link Falling" class="fallinglink z-10 not-md:invisible" />
 	<div class="flex w-3/4">
-		<div class="animate-bounce md:pt-17">
-			<img src="gojo-frolic.gif" alt="our glorious king frolicking" class="walkinggojo" />
+		<div class="w-full animate-bounce pt-17">
+			<img src="gojo-frolic.gif" alt="our glorious king frolicking" class="walkinggojo sticky aspect-square md:w-1/6" />
 		</div>
-		<div class="w-full justify-items-center not-md:pt-30">
-			<h1 class="comic-sans text-5xl font-extrabold">WELCOME TO MY EVIL LIER</h1>
+		<div class="w-full justify-items-center pt-6">
+			<h1 class="comic-sans text-center text-5xl font-extrabold">WELCOME TO MY EVIL LAIR</h1>
+			<div class="absolute justify-items-end not-md:invisible">
+				<img src="gojo-tweak.gif" alt="Gojo Tweaking" class=" z-20 animate-spin rounded-full p-10 md:absolute" />
+				<img src="fire.gif" alt="fire" class="sticky z-10 flex w-1/2" />
+			</div>
 		</div>
 	</div>
 
-	<div class="inline-flex w-full pt-6">
-		<h2 class="comic-sans text-3xl font-bold">Hello, I am Ice and</h2>
+	<div class="inline-flex w-3/4 gap-1 pt-6">
+		<h2 class="comic-sans text-end text-3xl font-bold text-nowrap">Hello, I am Ice and</h2>
 		<img class="w-2/7 md:w-1/7" src="/graphic-design-is-my-passion.webp" alt="Graphic Design is My Passion" />
 	</div>
 
 	<h2 class="mt-3 ml-20 text-3xl font-bold md:ml-70">Here are 5 reasons you should hire me:</h2>
 
 	<ol class="w-full list-decimal pt-10 md:pt-3">
-		<li class="comic-sans w-75 justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 text-5xl">1. I can center a div</li>
-
+		<div class="w-3/4">
+			<li class="comic-sans justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 text-5xl not-md:w-1/2">I can center a div</li>
+		</div>
 		<li class="comic-sans mr-10 w-1/2 justify-self-end rounded-3xl border-2 border-amber-600 bg-amber-400 text-5xl text-wrap md:w-1/3">
 			3. My styles are consistent
 		</li>
@@ -52,13 +58,18 @@
 
 	<div class="my-3 flex justify-center">
 		<a
-			class="comic-sans w-1/2 cursor-pointer justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 p-4 text-5xl md:w-1/3"
+			class="comic-sans w-1/2 cursor-pointer justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 p-4 text-center text-5xl md:w-1/3"
 			id="codeworks"
 			title="mycodeworks"
 			href="broke"
 		>
 			My code always works too!
 		</a>
+	</div>
+	<div
+		class="comic-sans my-3 flex w-full justify-center justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 p-4 text-center text-5xl md:w-1/3"
+	>
+		And I never over complicate my pages!
 	</div>
 
 	<div class="py-4">
--- src/routes/serious/+page.svelte
+++ src/routes/serious/+page.svelte
@@ -1,19 +1,26 @@
-<div class="bg-gray-500 pb-3 md:p-3">
+<svelte:head>
+	<title>Supern-Ice Portfolio</title>
+</svelte:head>
+
+<div class="bg-[#450693] pb-3 md:p-2">
 	<div
 		class="grid w-full grid-cols-4 gap-6 justify-self-center rounded-b-2xl bg-purple-500 p-5 shadow-2xl md:m-2 md:w-1/3 md:justify-self-start md:rounded-3xl"
 	>
 		<img src="alfie-ferret.png" alt="My GitHub Profile" class="col-span-4 row-span-4 justify-self-center rounded-full" />
+		<h1 class="col-span-4 row-span-1 text-center text-3xl font-bold">Hello, I'm Alfie!</h1>
+		<h2 class="col-span-4 row-span-1 text-center text-3xl font-semibold">You may also know me as Ice</h2>
+
 		<a
 			target="_blank"
 			href="https://github.com/5upern1ce"
-			class="col-span-2 row-span-3 flex flex-wrap justify-center justify-self-center rounded-sm border-2 bg-blue-300 p-3 align-middle text-2xl font-semibold"
-			>My GitHub <img src="github-mark.svg" alt="GitHub" class="aspect-square w-1/2 p-1" />
+			class="text-bg-center col-span-2 row-span-2 inline-grid h-full w-4/5 flex-1 flex-wrap justify-center gap-2 justify-self-center rounded-lg border-4 border-[#6F00FF] bg-[#FFF1F1] p-3 align-middle text-2xl font-semibold transition hover:border-[#9f57fd]"
+			>My GitHub <img src="github-mark.svg" alt="GitHub" class=" aspect-square w-full p-5 align-middle" />
 		</a>
 
 		<a
 			href="https://github.com"
-			class="col-span-2 row-span-3 flex flex-wrap justify-center justify-self-center rounded-sm border-2 bg-blue-300 p-3 align-middle text-2xl font-semibold"
-			>My GitHub <img src="github-mark.svg" alt="GitHub" class="aspect-square w-1/2 p-1" />
+			class="col-span-2 row-span-2 inline-grid aspect-square h-full w-4/5 flex-wrap justify-center justify-self-center rounded-lg border-4 border-[#6F00FF] bg-[#FFF1F1] p-3 text-center align-middle text-2xl font-semibold transition hover:border-[#9f57fd]"
+			>My CV <img src="cv.svg" alt="GitHub" class="aspect-square w-full pt-1 align-middle text-gray-500 not-md:mt-7 md:p-5 md:pb-12" />
 		</a>
 	</div>
 </div>
