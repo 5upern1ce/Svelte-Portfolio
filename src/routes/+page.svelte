b6f0f58de2ab1986ed06c8520f08113ad872ce51
sions5801 <simmssion@gmail.com>
2025-12-10 10:40:55 +0000

    Started darkfantasy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,6 +49,7 @@
 		<div class="pl-10">
 			<li class="comic-sans my-3 inline-flex justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 text-5xl md:w-1/3">
 				I am bad to the bone (do NOT click on the skeleton)
+
 				<button on:click={badtothebone} class="w-3/4 cursor-pointer md:h-full md:w-1/2">
 					<img src="smilingskele.gif" alt="Rad smiling skeleton" class="rounded-3xl align-middle" />
 				</button>
@@ -66,13 +67,21 @@
 			My code always works too!
 		</a>
 	</div>
+
 	<div
 		class="comic-sans my-3 flex w-full justify-center justify-self-center rounded-3xl border-2 border-amber-600 bg-amber-400 p-4 text-center text-5xl md:w-1/3"
 	>
 		And I never over complicate my pages!
 	</div>
 
-	<div class="py-4">
+	<div class="justify-self-center md:w-1/7">
+		<a href="darkfantasy" class="justify-center">
+			<h1 class="comic-sans text-2xl font-semibold">Come on an Adventure</h1>
+			<img src="darkfantasy/knightslumpedover.jpg" alt="Dark Fantasy Knight" class="w-full rounded-lg" />
+		</a>
+	</div>
+
+	<div class="py-3">
 		<img src="walkingskele.gif" alt="Skele walking gif" class=" walkingskele" />
 	</div>
 </div>
---
+++ src/routes/darkfantasy/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	// var audio = new Audio('bad-to-the-bone.mp3');
+	// var UserInteracted = setInterval(() => {
+	// 	audio
+	// 		.play()
+	// 		.then(() => {
+	// 			clearInterval(UserInteracted);
+	// 		})
+	// 		.catch(() => {
+	// 			console.log('waiting for user interaction to play first notification');
+	// 		});
+	// }, 1000);
+</script>
+
+<svelte:body />
+
+<div class="h-full w-full">
+	<h1 class="fantasy m-2 text-center text-5xl font-semibold italic">Welcome Traveller</h1>
+	<br />
+</div>
